e4da3f11c60ee9d74d776be584f6f8c04d3c00a4
Jefferson25-pog <jefferson272910@gmail.com>
2025-11-17 16:17:00 +0530

    Filename turned to lowercase

--- clinicproject/clinicproject/settings.py
+++ clinicproject/clinicproject/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'AdminApp',
-    'LabTechApp',
-    'PharmacistApp',
-    'DoctorApp',
-    'ReceptionistApp',
+    'adminapp',
+    'doctorapp',
+    'pharmacistapp',
+    'doctorapp',
+    'receptionistapp',
 ]
 
 MIDDLEWARE = [
